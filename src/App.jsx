11687ad0590d1e5c94656fb34f16a37fd800a3b3
TapZe <nabilmuyassar@Gmail.com>
2024-09-27 13:34:32 +0700

    feat: add routeprovider in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
 function App() {
   return (
     <>
-      <button
-        className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

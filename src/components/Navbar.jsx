d845ee56b6ba0b7db9f10a020dbec32a81560ccf
TapZe <nabilmuyassar@Gmail.com>
2024-09-27 15:07:02 +0700

    feat: adding Link into navbar, updating navbar and footer styling, add ThemeController component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+import ThemeController from "./ThemeController";
+
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100">
+    <div className="pt-3 px-5">
+      <div className="navbar bg-base-100 shadow-xl rounded-box">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -25,51 +28,50 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a>Item 1</a>
+                <Link to={`/`}>Home</Link>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <Link to={`/about`}>About</Link>
               </li>
               <li>
-                <a>Item 3</a>
+                <Link to={`/experience`}>Experience</Link>
+              </li>
+              <li>
+                <Link to={`/skills`}>Skills</Link>
+              </li>
+              <li>
+                <Link to={`/interest`}>Interest</Link>
+              </li>
+              <li>
+                <Link to={`/awards`}>Awards</Link>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link to={`/`} className="btn btn-ghost text-xl">
+            M Nabil Muyassar R
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
+              <Link to={`/about`}>About</Link>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <Link to={`/experience`}>Experience</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link to={`/skills`}>Skills</Link>
+            </li>
+            <li>
+              <Link to={`/interest`}>Interest</Link>
+            </li>
+            <li>
+              <Link to={`/awards`}>Awards</Link>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <ThemeController />
         </div>
       </div>
     </div>

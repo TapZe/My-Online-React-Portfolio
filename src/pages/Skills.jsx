aa8dd374510af7debdaf9073530744da0cb73612
TapZe <nabilmuyassar@Gmail.com>
2024-09-28 21:47:06 +0700

    feat: finalizing skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,18 @@
+import SkillCard from "../components/SkillCard";
+import skillsData from "../data/SkillsData";
+
 const Skills = () => {
   return (
-    <div>
-      <h1>Skills</h1>
-    </div>
+    <main className="flex-grow flex self-center items-center my-4 md:px-32 lg:px-48 xl:px-56">
+      <div className="w-full">
+        <h1 className="text-4xl font-bold text-center mb-10">My Skills</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-6">
+          {skillsData.map((skill) => (
+            <SkillCard key={skill.id} {...skill} />
+          ))}
+        </div>
+      </div>
+    </main>
   );
 };
 

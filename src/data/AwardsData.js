f5b288e24f71ae8cb4654c8a5657fc707f146be7
TapZe <nabilmuyassar@Gmail.com>
2024-09-28 21:45:58 +0700

    feat: setting and adding awards and skills data

---
+++ src/data/AwardsData.js
@@ -0,0 +1,44 @@
+const awardsData = [
+  {
+    credId: "72202 2132 02716 2022",
+    title: "Junior Web Programmer",
+    year: "2022",
+    link: "https://drive.google.com/file/d/1NcGlsFxpjdCOVhIULO-gFcKKy8qGKaBV/view?usp=drive_link",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdHBb4i_YPZiDASeyE1vBIVE8D0dGPVcQuYA&s", // Replace with actual image URL
+  },
+  {
+    credId: "410d7f4c-a07d-472d-873e-520cb710c64e",
+    title: "Artificial Intelligence BIZ (AIBIZ)",
+    year: "2023",
+    link: "https://www.credential.net/410d7f4c-a07d-472d-873e-520cb710c64e",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs-xmuYCqFEwpctA7cmTpY3v8p_IeDTlF3Hw&s", // Replace with actual image URL
+  },
+  {
+    credId: "2006927840-111",
+    title: "Game dan Animasi (Talent Scouting Academy)",
+    year: "2023",
+    link: "https://digitalent.kominfo.go.id/cek-sertifikat",
+    image:
+      "https://aptika.kominfo.go.id/wp-content/uploads/2019/09/digitalent-e1568964668804.jpg", // Replace with actual image URL
+  },
+  {
+    credId: "72102 3121 00998 2023",
+    title: "Junior Office Operator",
+    year: "2023",
+    link: "https://drive.google.com/file/d/1b4E_vs_Z9bybKZmxbVGrh5n2AwJ0uUR7/view?usp=drive_link",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdHBb4i_YPZiDASeyE1vBIVE8D0dGPVcQuYA&s", // Replace with actual image URL
+  },
+  {
+    credId: "tapze-bedaa",
+    title: "Back End Development and APIs",
+    year: "2024",
+    link: "https://freecodecamp.org/certification/TapZe/back-end-development-and-apis",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/3/39/FreeCodeCamp_logo.png", // Replace with actual image URL
+  },
+];
+
+export default awardsData;
---
+++ src/data/SkillsData.js
@@ -0,0 +1,42 @@
+const skillsData = [
+  {
+    id: 1,
+    name: "JavaScript",
+    description: "A versatile programming language for web development.",
+    image: "https://skillicons.dev/icons?i=js",
+  },
+  {
+    id: 2,
+    name: "Python",
+    description:
+      "A powerful programming language used for various applications.",
+    image: "https://skillicons.dev/icons?i=py",
+  },
+  {
+    id: 3,
+    name: "React",
+    description: "A JavaScript library for building user interfaces.",
+    image: "https://skillicons.dev/icons?i=react",
+  },
+  {
+    id: 4,
+    name: "Node.js",
+    description: "JavaScript runtime built on Chrome's V8 JavaScript engine.",
+    image: "https://skillicons.dev/icons?i=nodejs",
+  },
+  {
+    id: 5,
+    name: "AWS",
+    description: "A comprehensive cloud computing platform provided by Amazon.",
+    image: "https://skillicons.dev/icons?i=aws",
+  },
+  {
+    id: 6,
+    name: "SQL",
+    description:
+      "A standard language for accessing and manipulating databases.",
+    image: "https://skillicons.dev/icons?i=mysql",
+  },
+];
+
+export default skillsData;
